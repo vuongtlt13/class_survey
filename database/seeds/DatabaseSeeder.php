1511a4e97c2830efb539844eaf00b18080aeac56
Nguyen Duc Vuong <32158927+whatever1211@users.noreply.github.com>
2018-12-28 10:45:12 +0700

    Update DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,9 +16,9 @@ class DatabaseSeeder extends Seeder
         UserController::create_admin('admin', '1', 1);
 
         QuestionController::createTitle('Đánh giá môn học');
-        QuestionController::createTitle('Cơ sở vật chất');
+        QuestionController::createTitle('Nhận xét về công tác phục vụ');
         QuestionController::createTitle('Hoạt động giảng dạy của giáo viên');
-        QuestionController::createTitle('Hiệu quả giảng dạy');
+        QuestionController::createTitle('Bản thân sinh viên');
 
 
         QuestionController::createQuestion(1, "Nội dung môn học thiết thực, hữu ích");
@@ -27,10 +27,16 @@ class DatabaseSeeder extends Seeder
         QuestionController::createQuestion(1, "Thời lượng môn học được phân bổ hợp lý cho các hình thức học tập");
         QuestionController::createQuestion(1, "Các tài liệu phục vụ môn học được cập nhật");
         QuestionController::createQuestion(1, "Bạn được hỗ trợ kịp thời trong quá trình học môn này");
+        QuestionController::createQuestion(1, "Học phần môn học đã kích thích được tư duy sáng tạo");
+        QuestionController::createQuestion(1, "Về tổng thể, tôi đánh giá học phần này ở mức độ tốt");
 
         QuestionController::createQuestion(2, "Giảng đường đáp ứng yêu cầu của môn học");
         QuestionController::createQuestion(2, "Các trang thiết bị tại giảng đường đáp ứng yêu cầu giảng dạy và học tập");
-
+        QuestionController::createQuestion(2, "Giảng đương được chuẩn bị và phục vụ ở mức độ tốt");
+        QuestionController::createQuestion(2, "Sỹ số lớp học hợp lý");
+        QuestionController::createQuestion(2, "Thư viện đã phục vụ tốt cho nhu cầu tham khảo của học phần");
+        QuestionController::createQuestion(2, "Về tổng thể, tôi đánh giá công tác phục vụ của học phần này ở mức độ tốt");
+        
         QuestionController::createQuestion(3, "Giảng viên đã thiết kế, tổ chức học phần và sử dụng thời gian một cách khoa học, hợp lí");
         QuestionController::createQuestion(3, "Giảng viên đến lớp khi đã chuẩn bị tốt bài giảng");
         QuestionController::createQuestion(3, "Tôi cảm thấy hứng thú trong giờ học");
@@ -47,10 +53,17 @@ class DatabaseSeeder extends Seeder
         QuestionController::createQuestion(3, "Giảng viên thường xuyên lên lớp đúng giờ và thực hiện đúng lịch giảng dạy theo quy định");
         QuestionController::createQuestion(3, "Giảng viên thể hiện sự thân thiện, cởi mở trong giao tiếp với người học");
         QuestionController::createQuestion(3, "Giảng viên đề cập và nhấn mạnh những thông tin quan trọng một cách rõ ràng, dễ hiểu");
+        QuestionController::createQuestion(3, "Giảng viên có hiểu biết sâu rộng về học phần/môn học");
+        QuestionController::createQuestion(3, "Giảng viên đã tổ chức và kiểm soát tốt lớp học");
+        QuestionController::createQuestion(3, "Giảng viên tôn trọng ý kiến của sinh viên");
+        QuestionController::createQuestion(3, "Giảng viên đánh giá một cách công bằng quá trình học của sinh viên");
+        QuestionController::createQuestion(3, "Về tổng thể, tôi đánh giá hoạt động giảng dạy của giảng viên ở mức độ tốt");
 
-
+        QuestionController::createQuestion(4, "Tôi đã dành sự cố gắng hết sức cho môn học này");
         QuestionController::createQuestion(4, "Tôi đã lĩnh hội được những kiến thức cơ bản của học phần");
         QuestionController::createQuestion(4, "Tôi đã đạt được các kĩ năng thực hành có thể cần thiết cho tương lai");
         QuestionController::createQuestion(4, "Thông qua hoạt động  giảng dạy của giảng viên, tôi càng đánh giá cao giá trị của học phần này");
+        QuestionController::createQuestion(4, "Tôi đã dành sự cố gắng hết sức cho môn học này");
+        QuestionController::createQuestion(4, "Tôi muốn điểm môn học này đạt loại");
     }
 }
